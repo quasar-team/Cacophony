b4df456983455b0cf1995affa3a60262ea19288f
Piotr Nikiel <piotr@nikiel.info>
2020-05-13 20:19:58 +0200

    Now the mapper is complete

--- generateStuff.py
+++ generateStuff.py
@@ -10,6 +10,41 @@ sys.path.insert(0, 'FrameworkInternals')
 
 from transformDesign import transformDesign
 
+# we use template_debug to print out to keep uniform debug style with what comes out as debug
+# from the transform itself
+from transform_filters import template_debug
+
+ObviousMapping = {  # these are unanimously good choices
+    'OpcUa_Boolean' : "DPEL_BOOL",
+    'OpcUa_UInt32'  : "DPEL_UINT",
+    'OpcUa_Int32'   : "DPEL_INT",
+    'OpcUa_UInt64'  : "DPEL_ULONG",
+    'OpcUa_Int64'   : "DPEL_LONG",
+    'OpcUa_Float'   : "DPEL_FLOAT",
+    'OpcUa_Double'  : "DPEL_FLOAT",
+    'UaString'      : "DPEL_STRING",
+    'UaByteString'  : "DPEL_BLOB"
+    }
+
+def quasar_data_type_to_dpt_type_constant(quasar_data_type):
+    if quasar_data_type in ObviousMapping:
+        return ObviousMapping[quasar_data_type]
+    # for the remaining types, we have to do less obvious choices.
+    elif quasar_data_type == 'OpcUa_Byte':
+        template_debug("WARNING: mapped OpcUa_Byte to DPEL_UINT, because of no corresponding type in WinCC OA.")
+        return "DPEL_UINT";
+    elif quasar_data_type == 'OpcUa_SByte':
+        template_debug("WARNING: mapped OpcUa_SByte to DPEL_INT, because of no corresponding type in WinCC OA.")
+        return "DPEL_INT";
+    elif quasar_data_type == 'OpcUa_UInt16':
+        template_debug("WARNING: mapped OpcUa_UInt16 to DPEL_UINT, because of no corresponding type in WinCC OA.")
+        return "DPEL_UINT";
+    elif quasar_data_type == 'OpcUa_Int16':
+        template_debug("WARNING: mapped OpcUa_Int16 to DPEL_INT, because of no corresponding type in WinCC OA.")
+        return "DPEL_INT";
+    else:
+        raise Exception("The following quasar datatype: '{0}' is not yet supported in Cacophony.".format(quasar_data_type))
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--dpt_prefix", dest="dpt_prefix", default="Quasar")
@@ -19,12 +54,14 @@ def main():
     parser.add_argument("--function_prefix", dest="function_prefix", default="")
     args = parser.parse_args()
 
-    additional_params = [ # TODO make it a dictionary
-        'typePrefix={0}'.format(args.dpt_prefix),
-        'serverName={0}'.format(args.server_name),
-        'driverNumber={0}'.format(args.driver_number),
-        'subscriptionName={0}'.format(args.subscription),
-        'functionPrefix={0}'.format(args.function_prefix)]
+    additional_params = {
+        'typePrefix'       : args.dpt_prefix,
+        'serverName'       : args.server_name,
+        'driverNumber'     : args.driver_number,
+        'subscriptionName' : args.subscription,
+        'functionPrefix'   : args.function_prefix,
+        'mapper'           : quasar_data_type_to_dpt_type_constant
+        }
 
     cacophony_root = os.path.dirname(os.path.sep.join([os.getcwd(), sys.argv[0]]))
     print('Cacophony root is at: ' + cacophony_root)
@@ -35,12 +72,12 @@ def main():
         astyleRun=True,
         additionalParam=additional_params)
 
-    transformDesign(
-        xsltTransformation=os.path.join(thisModuleName, 'xslt', 'designToConfigParser.xslt'),
-        outputFile=os.path.join(thisModuleName, 'generated', 'configParser.ctl'),
-        requiresMerge=False,
-        astyleRun=True,
-        additionalParam=additional_params)
+    # transformDesign(
+    #     xsltTransformation=os.path.join(thisModuleName, 'xslt', 'designToConfigParser.xslt'),
+    #     outputFile=os.path.join(thisModuleName, 'generated', 'configParser.ctl'),
+    #     requiresMerge=False,
+    #     astyleRun=True,
+    #     additionalParam=additional_params)
 
 if __name__ == "__main__":
     main()
